907d49778a36440a8bbb56c6ebad7fd7700454a2
mohahmadi2001 <moh.ahmdi2001@gmail.com>
2023-05-14 17:47:20 +0330

    check password

--- homework/homework4/users.py
+++ homework/homework4/users.py
@@ -5,16 +5,27 @@ class User:
     users = []
     
     def __init__(self, username, password, phone_number = None) :
-        if username in User.users:
+        if username in self.users:
             raise ValueError("This username already exists.")
         self.username = username
         self.users.append(username)
+        self.__password = None
+        self.password = password
+        
+    @property
+    def password(self):
+        return self.__password
     
-    
+    @password.setter
+    def password(self, password):
+        if len(password) < 4:
+            raise ValueError("Password must be at least 4 characters long.")
+        self.__password = password
+        
+    def __str__(self) -> str:
+        return f"{self.username} : {self.__password}"
 
-u1 = User("alii")
-u2 = User("ali")
+u1 = User("mohammad","1234")
+u2 = User("ali","4321")
 print(u1)
 print(u2)
-    
-    
