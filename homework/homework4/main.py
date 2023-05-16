92879b972c0b7033d1330098f1d0d07e97d8670b
mohahmadi2001 <moh.ahmdi2001@gmail.com>
2023-05-16 18:17:35 +0330

    use uuid package to generate unique ID for users

--- homework/homework4/main.py
+++ homework/homework4/main.py
@@ -1,12 +1,21 @@
+"""
+In this module, the previously created user class is used
+"""
 from users import User
 
 def register_user():
+    """
+    In this function, user registering user is checked
+    """
     username = input("Enter a username: ")
     password = input("Enter a password (at least 4 characters): ")
     phone_number = input("Enter a phone number (optional): ")
     User(username,password,phone_number)
 
 def login_user():
+    """
+    In this function, user login is checked
+    """
     username = input("Enter your username: ")
     password = input("Enter your password: ")
     
@@ -22,6 +31,9 @@ def login_user():
     
 
 def edit_user():
+    """
+    In this function, edit user is checked
+    """
     print("1. Edit username")
     print("2. Edit phone number")
     choice = int(input("Enter choice: "))
@@ -42,6 +54,9 @@ def edit_user():
         print("Invalid choice.")
 
 def change_password():
+    """
+    In this function, user can change password
+    """
     old_password = input("Enter old password: ")
     new_password = input("Enter new password: ")
     confirm_password = input("Confirm new password: ")
@@ -84,110 +99,3 @@ while True:
         break
     else:
         print("Invalid choice.")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# users = {}
-
-# def register_user():
-#     username = input("Enter a username: ")
-#     password = input("Enter a password (at least 4 characters): ")
-#     phone_number = input("Enter a phone number (optional): ")
-
-#     new_user = User(username,password,phone_number)
-#     users[new_user] = new_user
-    
-#     print(users[new_user])
-    
-# def login_user():
-#     username = input("Enter your username: ")
-#     password = input("Enter your password: ")
-    
-#     if username not in users:
-#         print("This username does not exist.")
-#         return
-#     user = users[username]
-
-#     if user.password != password:
-#         print("Incorrect password.")
-#         return
-    
-#     while True:
-#         choice = input("Enter 1 to view your profile, 2 to edit your profile, 3 to change your password, or 4 to log out: ")
-        
-#         if choice == "1":
-#             print(user)
-#         elif choice == "2":
-#             new_username = input("Enter a new username (or press enter to keep the current one): ")
-#             if new_username:
-#                 users.pop(username)
-#                 user.username = new_username
-#                 users[username] = user
-#             new_phone_number = input("Enter a new phone number (or press enter to keep the current one): ")
-#             user.phone_number = new_phone_number
-#         elif choice == "3":
-#             old_password = input("Enter your old password: ")
-#             new_password = input("Enter a new password (at least 4 characters): ")
-#             confirm_password = input("Enter the new password again: ")
-            
-#             if old_password != user.password:
-#                 print("Incorrect password.")
-#                 continue
-#             if new_password != confirm_password:
-#                 print("Passwords do not match.")
-#                 continue
-#             user.password = new_password
-#             print("Password changed successfully.")
-            
-#         elif choice == "4":
-#             return
-#         else:
-#             print("Invalid choice.")
-            
-            
-# while True:
-#     choice = input("Enter 1 to register a new user, 2 to log in, or 0 to exit: ")
-#     if choice == "1":
-#         register_user()
-    # elif choice == "2":
-    #     login_user()
-    # elif choice == "0":
-    #     break
-    # else:
-    #     print("Invalid choice.")
-               
-                
-    
-    
-    
--- homework/homework4/users.py
+++ homework/homework4/users.py
@@ -1,9 +1,13 @@
-
+import uuid
+"""
+    In this module, a user class is created that stores user information
+    including username, password and phone number
+"""
 class User:
-    id_counter = 0
+    # id_counter = 0
     users = {}
     
-    def __init__(self, username, password, phone_number = None) :
+    def __init__(self, username: str, password: str, phone_number : int = None) :
         if username in self.users:
             raise ValueError("This username already exists.")
         self.username = username
@@ -11,22 +15,38 @@ class User:
         self.__password = None
         self.password = password
         self.phone_number = phone_number
-        User.id_counter += 1
-        self.id = User.id_counter
+        # User.id_counter += 1
+        # self.id = User.id_counter
+        self.id = uuid.uuid4().int
        
         
     @property
-    def password(self):
+    def password(self) -> str:
         return self.__password
     
     @password.setter
-    def password(self, password):
+    def password(self, password : str):
         if len(password) < 4:
             raise ValueError("Password must be at least 4 characters long.")
         self.__password = password
         
     @staticmethod
-    def valid_password(old_password,new_password,confirm_password):
+    def valid_password(old_password: int,new_password: int,confirm_password: int) -> int:
+        """
+        In this function, the user's password is validated
+
+        Args:
+            old_password (int): old password
+            new_password (int): new password
+            confirm_password (int): confirm password
+
+        Raises:
+            ValueError: if old password is incorrect.
+            ValueError: if new passwords do not match.
+
+        Returns:
+            int: new password
+        """
         if old_password != User.current_user.password:
             raise ValueError("Old password is incorrect.")
         if new_password != confirm_password:
